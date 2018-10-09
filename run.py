9e40a679765fbeb9df7527af46b06f9fd520fc43
KagenoMoheji <reeg.gold.verecrms@gmail.com>
2018-10-09 16:15:04 +0900

    フロントサイドのテスト用レンダリング追加

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
-# aaaaaa
+from modules import Modules
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,5 +12,13 @@ def hello_python():
     message = "Hello Python!"
     return render_template("test.html", message = message)
 
+@app.rout("/front-test")
+def front-test():
+    return render_template("front-test.html")
+
+'''
+@app.route("/test_postgresql")
+def hello_postgresql():pass
+'''
 if __name__ == "__main__":
     app.run()
