94ffa5f1eff6967312f298848c6c79154c73b976
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2018-10-15 15:56:41 +0900

    postgresql-test push

---
+++ modules.py
@@ -0,0 +1,58 @@
+import os
+import psycopg2
+
+class SensorProcess():
+    def __init__(self):pass
+    def insertSensorData(self):
+        dp = DBProcess()
+        conn = dp.getDBConn()
+        cursor = conn.cursor()
+        #cursor.execute("insert ")
+        #conn.commit()
+        dp.closeConn(cursor, conn)
+
+'''
+http://h2shiki.hateblo.jp/entry/2016/05/05/210738
+'''
+class DBProcess():
+    def __init__(self):
+        self.tableName = "nagara"
+    
+    def getDBConn(self):
+        return psycopg2.connect(
+            host = DB_HOSTNAME,
+            database = DB_DATABASE,
+            port = DB_PORT,
+            user = DB_USER,
+            password = DB_PASSWORD
+        )
+    
+    def closeConn(self, cursor, conn):
+        cursor.close()
+        conn.close()
+    
+    def dbInsert(self):
+        pass
+    
+    def dbSelect(self, attr):
+        '''
+        [入力]
+        ●"項目1,項目2,..."
+        ●すべての項目を取得する場合は"*"のみ．
+        '''
+        conn = self.getDBConn()
+        cursor = conn.cursor()
+        cursor.execute("select "+ str + " from " + self.tableName)
+        result = cursor.fetchall()
+        self.closeConn(cursor, conn)
+        return result
+
+class BasicProcess():
+    def __init__(self):
+        self.dp = DBProcess()
+        #self.sp = SensorProcess()
+    
+    def getDBCalorie(self):
+        calorie = self.dp.dbSelect("calorie")
+    
+    #def shapeData():
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
-# aaaaaa
+from modules import DBProcess
+from modules import BasicProcess
+
 app = Flask(__name__)
+dp = DBProcess()
 
 @app.route("/")
 def hello_world():
@@ -10,6 +13,17 @@ def hello_world():
 def hello_python():
     message = "Hello Python!"
     return render_template("test.html", message = message)
+
+@app.route("/front-test")
+def front_test():
+    return render_template("front-test.html")
+
+
+@app.route("/test_postgresql")
+def hello_postgresql():
+    result = dp.dbSelect("*")
+    return result
+
 
 if __name__ == "__main__":
     app.run()
