2a9cdb4b771abb55d9c4d314c1c5cca53ba1090a
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2018-10-26 19:29:10 +0900

    check front-back connection

--- modules.py
+++ modules.py
@@ -119,7 +119,6 @@ class BasicProcess():
         sum = 0
     
         for num in range(len(DB_data)-1):
-            currentDay  =  DB_data[num]["datetime"].day
             nextDay = DB_data[num+1]["datetime"].day
             sum += DB_data[num]["calorie"] # カロリーを加算
             if currentDay != nextDay: # 日が違った場合
@@ -135,7 +134,7 @@ class BasicProcess():
                 sum += DB_data[num+1]["calorie"]
                 caloriePerDay[date] = sum
             
-        return caloriePerDay, currentDay
+        return caloriePerDay, date
 
     def get15minConsumedCaloire(self, DB_data):
         '''
