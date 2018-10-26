eda8b72980ae64b6235445813882f20af9bdcc38
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2018-10-27 01:30:07 +0900

    check front-back connection

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,7 @@
-$(function(){
-    $(".tab_label").on("click", function(){
-        var $tabIndex = $(this).index()
-        $(".tab_label").removeClass("active")
-        $(".tab_panel").removeClass("active")
-        $(this).addClass("active")
-        $(".tab_panel").eq($tabIndex).addClass("active")
-    })
+$(".tab_label").on("click", function(){
+    var $tabIndex = $(this).index()
+    $(".tab_label").removeClass("active")
+    $(".tab_panel").removeClass("active")
+    $(this).addClass("active")
+    $(".tab_panel").eq($tabIndex).addClass("active")
 })
