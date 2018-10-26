a64a320bb7bebb61e7a4d185a8a53ef5b02319ed
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2018-10-27 01:24:09 +0900

    check front-back connection

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".tab_label").on("click", function(){
+        var $tabIndex = $(this).index()
+        $(".tab_label").removeClass("active")
+        $(".tab_panel").removeClass("active")
+        $(this).addClass("active")
+        $(".tab_panel").eq($tabIndex).addClass("active")
+    })
+})
