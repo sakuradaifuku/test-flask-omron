88d9b8ffaa08f32ce70d5cb207d7016c0f452bd8
ohdoit2018 <43811586+ohdoit2018@users.noreply.github.com>
2018-10-27 11:27:03 +0900

    Update sample.js

--- static/js/sample.js
+++ static/js/sample.js
@@ -1,16 +1,1 @@
-//ムービー全画面スクリプト（PC用）
-//(1)動画の画角比率を設定します。4:3の場合はここを「4/3」に変更
-var movieRatio = 4/3;  
-//(2)画像のリサイズ関数「movieAdjust()」を作成
-function movieAdjust(){
-        var adjustWidth = $(window).width();
-        var adjustHeight = $(window).height();
-        if (adjustHeight > adjustWidth / movieRatio) {
-                adjustWidth = adjustHeight * movieRatio;
-        }
-        $('iframe').css({width:(adjustWidth),height:(adjustWidth/movieRatio)});
-}
-//(3)画面リサイズ時と画面ロード時に関数movieAdjust()を実行
-  $(window).on('load resize', function(){
-    movieAdjust();
-  });
+
