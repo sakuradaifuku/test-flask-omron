8f7f25fa600c10f2fffd877f27e987ebc1f5c09d
ohdoit2018 <43811586+ohdoit2018@users.noreply.github.com>
2018-10-27 11:21:18 +0900

    Update sample.js

--- static/js/sample.js
+++ static/js/sample.js
@@ -1,40 +1,16 @@
-(function($) {
-
-$(function() {
-    MovieCentering.init();
-});
-
-var MovieCentering = {
-    init: function(){
-        $(window).on('load', function(){
-            MovieCentering .resize();
-        });
-
-        $(window).on('resize', function(){
-            MovieCentering .resize();
-        });
-    }
-
-    ,resize: function(){
-        var windowSizeHeight = $('window').outerHeight()
-        ,windowSizeWidth = $('window').outerWidth();
-
-        var windowMovieSizeWidth = $('.BackVideo').outerWidth()
-        ,windowMovieSizeHeight = $('.BackVideo').outerHeight();
-
-        if (windowSizeWidth < windowMovieSizeWidth) {
-            var windowMovieSizeWidthLeftMargin = (windowMovieSizeWidth - windowSizeWidth) / 2;
-            $('.BackVideo').css({left: -windowMovieSizeWidthLeftMargin});
-        }else{
-            $('.BackVideo').css({left: 0});
+//ムービー全画面スクリプト（PC用）
+//(1)動画の画角比率を設定します。4:3の場合はここを「4/3」に変更
+var movieRatio = 16/9;  
+//(2)画像のリサイズ関数「movieAdjust()」を作成
+function movieAdjust(){
+        var adjustWidth = $(window).width();
+        var adjustHeight = $(window).height();
+        if (adjustHeight > adjustWidth / movieRatio) {
+                adjustWidth = adjustHeight * movieRatio;
         }
-
-        if (windowSizeWidth < windowMovieSizeWidth) {
-            var windowMovieSizeWidthLeftMargin = (windowMovieSizeWidth - windowSizeWidth) / 2;
-            $('.BackVideo').css({left: -windowMovieSizeWidthLeftMargin});
-        }else{
-            $('.BackVideo').css({left: 0});
-        }
-    }
+        $('iframe').css({width:(adjustWidth),height:(adjustWidth/movieRatio)});
 }
-})(jQuery);
+//(3)画面リサイズ時と画面ロード時に関数movieAdjust()を実行
+  $(window).on('load resize', function(){
+    movieAdjust();
+  });
