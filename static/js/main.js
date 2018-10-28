337cdd69ba3892af9202cf355f7ce7cb9aedc61c
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2018-10-28 20:18:03 +0900

    add fitbit-package, and modal-window test

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,41 @@
 $(function(){
-    var tablist = $(".tab_area li")
-    var tabbody = $(".panel_area li")
-
-    tablist.click(function(){
+    $(".tab_area li").click(function(){
+        var tabbody = $(".panel_area li")
         var idx = tablist.index($(this))
         tablist.removeClass("active").eq(idx).addClass("active")
         tabbody.removeClass("active").eq(idx).addClass("active")
+    })
+
+
+    $("modal_open").click(function(){
+        // モーダルウィンドウの黒背景のタグ挿入
+        $("body").append('<div class="modal_bg"></div>')
+        // 画面中央を算出
+        modalResize()
+
+        // モーダルウィンドウ出現
+        $(".modal_bg, .modal_window").fadeIn("slow")
+        // 閉じる処理
+        $(".modal_bg").click(function(){
+            $(".modal_bg, .modal_window").fadeOut("slow", function(){
+                // 挿入したタグも一緒に消す
+                $(".modal_bg").remove()
+            })
+        })
+
+
+        //画面中央を算出する処理
+        $(window).resize(modalResize)
+        function modalResize(){
+            var w = $(window).width()
+            var h = $(window).height()
+            var modalW = $(".modal_window").outerWidth()
+            var modalH = $(".modal_window").outerHeight()
+
+            $(".modal_window").css({
+                "left": ((w-modalW)/2) + "px",
+                "top": ((h-modalH)/2) + "px"
+            })
+        }
     })
 })
